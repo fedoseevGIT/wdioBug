c7d538b24e0cc9b159b345d993b6ba67cb0c0144
epszaw <konstantin@epishev.me>
2023-08-25 13:45:00 +0200

    proof of work for new steps api

--- test/specs/EmptyAllure.ts
+++ test/specs/EmptyAllure.ts
@@ -1,13 +1,12 @@
 import AllureReporter from '@wdio/allure-reporter';
-import {Status} from 'allure-js-commons';
 
 describe('Example test', () => {
     beforeEach(async () => {
         console.log('beforeEach');
 	});
+
     it('Empty allure', async () => {
-        AllureReporter.startStep('test');
-        AllureReporter.endStep(Status.PASSED);
+        await AllureReporter.step('test', () => {})
     })
 })
 
